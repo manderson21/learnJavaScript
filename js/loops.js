ee06d13ac4e6a51633e3b3c20dffbe7710b5227c
Matthew Anderson <anderson.c.matt@gmail.com>
2019-10-01 13:13:22 -0500

    made changes to loop and switchcase

---
+++ js/loops.js
@@ -0,0 +1,21 @@
+// For loops
+for (let i = 1; i <= 5; i++) {
+    console.log('For: Hello World!')
+}
+
+
+// While Loops
+let i = 0;
+while (i < 5) {
+    console.log('While: Hello World');
+    i++;
+}
+
+
+
+// do-while loops
+let I = 0;          // capital I just because i is taken
+do {
+    console.log('Do: Hello World!')
+    I++
+} while (I < 5);
