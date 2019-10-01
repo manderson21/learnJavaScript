4f711c8669c6f48ebb32f6d1d3cdfc6acabdd883
Matthew Anderson <anderson.c.matt@gmail.com>
2019-10-01 12:51:25 -0500

    added the loops and switch case tutorial

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-let firstName = 'Matthew';
-let lastName = 'Anderson';
-let age = 21;
+// Hour
+// If hour is between 6am and 12pm: Good Morning!
+// If hour is between 12pm and 6pm: Good Afternoon!
+// Otherwise we'll display: Good Evening!
 
-let person = {
-    firstName = 'Matthew',
-    lastName = 'Anderson',
-    age = 21
-}
+let currentTime = 19;
+
+if (currentTime >= 6 && currentTime < 12)
+    console.log("Good Morning!");
+else if (currentTime >= 12 && currentTime < 18)
+    console.log("Good Afternoon!");
+else 
+    console.log("Good Evening!");
