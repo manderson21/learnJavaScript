769e06d78f20cc2f24a7f2efe215771dcbe52c6c
Matthew Anderson <anderson.c.matt@gmail.com>
2019-10-21 13:56:15 -0500

    created a sub folder called Lesson 1 and added all lesson files to it

--- js/aritmeticOperators.js
+++
@@ -1,7 +0,0 @@
-let x = 10;
-let y = 3;
-
-console.log(x + y);     // addition operator
-console.log(x - y);     // subtraction operator
-console.log(x * y);     // multiplication operator
-console.log(x / y);     // division operator
--- js/arrrays.js
+++
@@ -1,5 +0,0 @@
-let selectedColors = ['red', 'blue'];
-
-console.log(selectedColors[1]);
-
-console.log(selectedColors.length);
